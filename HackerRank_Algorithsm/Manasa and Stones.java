088ce4e84a9ea7b8ab5239f790a6345a44c21c51
DuyHuynhMinh <39339988+DuyHuynhMinh@users.noreply.github.com>
2018-05-26 16:56:13 +0700

    HackerRank

---
+++ HackerRank_Algorithsm/Manasa and Stones.java
@@ -0,0 +1,67 @@
+/* https://www.hackerrank.com/challenges/manasa-and-stones/problem */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the stones function below.
+    static int[] stones(int n, int a, int b) {
+        
+        Set<Integer> set = new TreeSet<>();
+        int k = n-1;
+        
+        for(int i =k; i >=0;--i) {
+            set.add(i *a+(n-1-i)*b);
+        }
+    
+        int [] result = new int[set.size()];
+        int i = 0;
+        for(int s : set) {
+            result[i++] = s;
+        }
+    
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int a = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int b = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] result = stones(n, a, b);
+
+            for (int i = 0; i < result.length; i++) {
+                bufferedWriter.write(String.valueOf(result[i]));
+
+                if (i != result.length - 1) {
+                    bufferedWriter.write(" ");
+                }
+            }
+
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
