a41300981e77228beb0d631445e147ba7cf36267
Huynh,Duy <hminhduy89@gmail.com>
2018-05-29 10:07:42 +0700

    algorithsm

---
+++ Algorithsm/RotationOfString.java
@@ -0,0 +1,22 @@
+package Main;
+
+public class Main {
+    public static void main(String []arg) {
+
+        String s1 = "abcdef";
+        String s2 ="efabcd";
+        System.out.println(isRotation(s1,s2));
+
+
+    }
+
+
+    public static boolean isRotation (String s1, String s2) {
+        if(s1.length()!=s2.length()) return false;
+        StringBuilder sb = new StringBuilder(s1);
+        sb.append(s1);
+        if(sb.toString().contains(s2)){
+            return true;
+        }
+        return false;
+    }
