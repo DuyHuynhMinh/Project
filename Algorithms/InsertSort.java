a66ddc5af003d2ab25d4640e40ab0caed981c8b1
DuyHuynhMinh <39339988+DuyHuynhMinh@users.noreply.github.com>
2018-07-04 15:31:02 +0700

    Algorithms

--- Algorithms/InsertSort.java
+++ Algorithms/InsertSort.java
@@ -2,7 +2,6 @@ static void insertSort(int []a) {
         for(int i =1 ; i < a.length;++i) {
             int j = i-1 ;
             int key = a[i];
-
             while(j>=0&&a[j]>key) {
                 a[j+1]=a[j];
                 --j;
